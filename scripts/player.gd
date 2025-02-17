2a0e02bf391c512a35ed0e2280c94f13acfc55b3
Jake Wheeler <jakewheeler17@gmail.com>
2025-02-17 12:02:47 -0500

    add double jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+const MAX_JUMP = 2
+var jump = 0
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
@@ -12,13 +15,32 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	jump_handler()
+			
 	var direction := Input.get_axis("move_left", "move_right")
 	
+	animation_handler(direction)
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+	
+func jump_handler() -> void:
+	if is_on_floor():
+		jump = 0
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		jump += 1
+		
+	if Input.is_action_just_pressed("jump") and not is_on_floor() and jump < MAX_JUMP:
+		velocity.y = JUMP_VELOCITY
+		jump += 1
+
+func animation_handler(direction: float) -> void:
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
@@ -32,9 +54,3 @@ func _physics_process(delta: float) -> void:
 	else:
 		animated_sprite.play("jump")
 	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
