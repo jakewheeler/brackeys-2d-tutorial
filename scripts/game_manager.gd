eddbb9e3247d1ad11afc5aba4b304a8fe86c64bf
Jake Wheeler <jakewheeler17@gmail.com>
2025-02-17 12:52:43 -0500

    use signals with code

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,7 +3,12 @@ extends Node
 var score = 0
 
 @onready var score_label: Label = $ScoreLabel
+@onready var coins: Node = $"../Coins"
 
-func add_point() -> void:
+func _ready() -> void:
+	for coin in coins.get_children():
+		coin.collected.connect(_add_point)
+
+func _add_point() -> void:
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins"
